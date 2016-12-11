f51d5e87435b6d2c39556adaa8dd14a3fc4d7b8b
elix-x <Elix-x@users.noreply.github.com>
2016-12-11 15:47:47 +0100

    Implemented default object serializer
    By default does not compile static and transient fields.

---
+++ src/main/java/code/elix_x/excomms/serialization/serializers/ObjectSerializer.java
@@ -0,0 +1,66 @@
+package code.elix_x.excomms.serialization.serializers;
+
+import java.util.Collections;
+
+import com.google.common.collect.ImmutableList;
+
+import code.elix_x.excomms.reflection.ReflectionHelper;
+import code.elix_x.excomms.reflection.ReflectionHelper.AClass;
+import code.elix_x.excomms.reflection.ReflectionHelper.AField;
+import code.elix_x.excomms.reflection.ReflectionHelper.Modifier;
+import code.elix_x.excomms.serialization.DVisitor;
+import code.elix_x.excomms.serialization.SVisitor;
+import code.elix_x.excomms.serialization.Serializer;
+import code.elix_x.excomms.serialization.SerializerMain;
+
+public class ObjectSerializer<GenD, GenS, SerM extends SerializerMain<GenD, GenS, SerM>> implements Serializer<GenD, GenD, GenS, GenS, SerM>{
+
+	private static final String CLASS = "#CLASS#";
+	
+	private final ImmutableList<ReflectionHelper.Modifier> blackModifiers;
+
+	public ObjectSerializer(Modifier... modifiers){
+		blackModifiers = ImmutableList.copyOf(modifiers);
+	}
+
+	public ObjectSerializer(){
+		this(Modifier.STATIC, Modifier.TRANSIENT);
+	}
+
+	@Override
+	public boolean acceptsS(GenD o){
+		return true;
+	}
+
+	@Override
+	public boolean acceptsD(GenS o, Class<GenS> clas){
+		return true;
+	}
+
+	@Override
+	public GenS serialize(SerM serializerMain, GenD o){
+		AClass<GenD> clas = new AClass<>((Class<GenD>) o.getClass());
+		SVisitor<GenD, GenS, GenS, SerM, String> visitor = serializerMain.visitorS(clas.get());
+		visitor.visit(serializerMain.serialze((GenD) clas.get()), CLASS);
+		for(AField<? super GenD, ?> field : clas.getFields()){
+			if(Collections.disjoint(blackModifiers, field.modifiers())){
+				visitor.visit(serializerMain.serialze((GenD) field.setAccessible(true).get(o)), field.get().getName());
+			}
+		}
+		return visitor.endVisit();
+	}
+
+	@Override
+	public GenD deserialize(SerM serializerMain, GenS o){
+		DVisitor<GenD, GenD, GenS, SerM, String> visitor = serializerMain.visitorD((Class<GenS>) o.getClass()); 
+		GenD genD = visitor.startVisit((Class<GenD>) serializerMain.deserialize(visitor.visit(CLASS), (Class<GenD>) Class.class));
+		AClass<GenD> clas = new AClass<GenD>((Class<GenD>) genD.getClass());
+		for(AField<? super GenD, ?> field : clas.getFields()){
+			if(Collections.disjoint(blackModifiers, field.modifiers())){
+				field.setAccessible(true).setFinal(false).set(genD, serializerMain.deserialize(visitor.visit(field.get().getName()), (Class<GenD>) field.get().getType()));
+			}
+		}
+		return genD;
+	}
+
+}
