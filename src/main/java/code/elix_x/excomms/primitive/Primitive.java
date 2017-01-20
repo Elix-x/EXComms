ff9a2f20c15399d09b34b9b0a0d58f046ba00295
Elix_x <Elix-x@users.noreply.github.com>
2017-01-20 13:19:34 +0100

    Implemented primitve class - it holds boxed primitives

---
+++ src/main/java/code/elix_x/excomms/primitive/Primitive.java
@@ -0,0 +1,29 @@
+package code.elix_x.excomms.primitive;
+
+public class Primitive<P> {
+
+	private final PrimitiveType type;
+	private P value;
+
+	public Primitive(PrimitiveType type){
+		this.type = type;
+	}
+
+	public Primitive(P value){
+		this(PrimitiveType.getPrimitive(value.getClass()));
+		this.value = value;
+	}
+
+	public P getValue(){
+		return value;
+	}
+
+	public void setValue(P value){
+		this.value = value;
+	}
+
+	public PrimitiveType getType(){
+		return type;
+	}
+
+}
