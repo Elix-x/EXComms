df2a392ace4c49d4721c6427dc2fde89f09bb93c
Elix_x <Elix-x@users.noreply.github.com>
2017-01-20 13:15:31 +0100

    Changed primitve to primitive type

--- src/main/java/code/elix_x/excomms/primitive/PrimitiveType.java
+++ src/main/java/code/elix_x/excomms/primitive/PrimitiveType.java
@@ -1,6 +1,6 @@
 package code.elix_x.excomms.primitive;
 
-public enum Primitive {
+public enum PrimitiveType {
 
 	BOOLEAN(boolean.class, Boolean.class, 1), BYTE(byte.class, Byte.class, Byte.SIZE), SHORT(short.class, Short.class, Short.SIZE), INT(int.class, Integer.class, Integer.SIZE), LONG(long.class, Long.class, Long.SIZE), FLOAT(float.class, Float.class, Float.SIZE), DOUBLE(double.class, Double.class, Double.SIZE);
 
@@ -9,7 +9,7 @@ public enum Primitive {
 
 	private final int bits;
 
-	private Primitive(Class<?> primitiveClass, Class<?> boxedClass, int bits){
+	private PrimitiveType(Class<?> primitiveClass, Class<?> boxedClass, int bits){
 		this.primitiveClass = primitiveClass;
 		this.boxedClass = boxedClass;
 		this.bits = bits;
@@ -31,8 +31,8 @@ public enum Primitive {
 		return (int) Math.ceil(bits / 8f);
 	}
 
-	public static Primitive getPrimitive(Class<?> clas){
-		for(Primitive primitive : values()){
+	public static PrimitiveType getPrimitive(Class<?> clas){
+		for(PrimitiveType primitive : values()){
 			if(primitive.primitiveClass == clas || primitive.boxedClass == clas) return primitive;
 		}
 		throw new IllegalArgumentException(String.format("Class %s is neither primitive type, nor boxed primitive type!", clas.getName()));
