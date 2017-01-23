b8d67e9f2e4baae88f2e612f2aefb88040583ba9
Elix_x <Elix-x@users.noreply.github.com>
2017-01-23 15:21:22 +0100

    Implemented primitive serialization methods
    Implemented primitive serialization methods in priitive binary
    serializer.

--- src/main/java/code/elix_x/excomms/serialization/visit/binary/BinaryPrimitveSerializer.java
+++ src/main/java/code/elix_x/excomms/serialization/visit/binary/BinaryPrimitveSerializer.java
@@ -25,13 +25,64 @@ public class BinaryPrimitveSerializer<P> implements Serializer<Object, Primitive
 	}
 
 	@Override
-		return null;
 	public ByteBuffer serialize(BinarySerializerMain serializerMain, Primitive<P> p){
+		ByteBuffer buffer = ByteBuffer.allocate(type.hashCode());
+		switch(type){
+			case BOOLEAN:
+				buffer.put((byte) (((Boolean) p.getValue()) == true ? 1 : 0));
+				break;
+			case BYTE:
+				buffer.put((byte) p.getValue());
+				break;
+			case SHORT:
+				buffer.putShort((short) p.getValue());
+				break;
+			case INT:
+				buffer.putInt((int) p.getValue());
+				break;
+			case LONG:
+				buffer.putLong((long) p.getValue());
+				break;
+			case FLOAT:
+				buffer.putFloat((float) p.getValue());
+				break;
+			case DOUBLE:
+				buffer.putDouble((double) p.getValue());
+				break;
+		}
+		return buffer;
 	}
 
 	@Override
-		return null;
 	public Primitive<P> deserialize(BinarySerializerMain serializerMain, ByteBuffer buffer){
+		Primitive p;
+		switch(type){
+			case BOOLEAN:
+				p = new Primitive<>(buffer.get() == 1);
+				break;
+			case BYTE:
+				p = new Primitive<>(buffer.get());
+				break;
+			case SHORT:
+				p = new Primitive<>(buffer.getShort());
+				break;
+			case INT:
+				p = new Primitive<>(buffer.getInt());
+				break;
+			case LONG:
+				p = new Primitive<>(buffer.getLong());
+				break;
+			case FLOAT:
+				p = new Primitive<>(buffer.getFloat());
+				break;
+			case DOUBLE:
+				p = new Primitive<>(buffer.getDouble());
+				break;
+			default:
+				p = null;
+				break;
+		}
+		return p;
 	}
 
 }
