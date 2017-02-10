5300803376732bf86234d701a9569dad4394bc61
Elix_x <Elix-x@users.noreply.github.com>
2017-02-10 13:21:25 +0100

    Finalized primitives test in binary serialization tests

--- src/test/java/test/elix_x/excomms/serialization/binary/BinarySerializationTest.java
+++ src/test/java/test/elix_x/excomms/serialization/binary/BinarySerializationTest.java
@@ -2,8 +2,8 @@ package test.elix_x.excomms.serialization.binary;
 
 import static org.junit.Assert.*;
 
-import java.nio.ByteBuffer;
 import java.util.Arrays;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
@@ -16,12 +16,34 @@ import code.elix_x.excomms.serialization.binary.serializer.BinaryPrimitiveSerial
 public class BinarySerializationTest {
 
 	@Test
-	public void test(){
+	public void testPrimitives(){
 		BinarySerializerMain serializer = new BinarySerializerMain(Arrays.stream(PrimitiveType.values()).flatMap(primitiveType -> Arrays.stream(new Serializer[]{new BinaryPrimitiveSerializer<>(primitiveType), new BinaryPrimitiveSerializer.ActualTypesWrapper<>(primitiveType)})).collect(Collectors.toList()));
 
-		int i = 154515;
-		ByteBuffer buffer = serializer.serialze(i);
-		assertEquals("Serialization - deserialization of an integer failed", i, (int) serializer.deserialize(buffer, int.class));
+		Random random = new Random();
+
+		boolean b = random.nextBoolean();
+		assertEquals("Serialization - deserialization of boolean failed", b, serializer.deserialize(serializer.serialze(b), boolean.class));
+
+		byte by = (byte) random.nextInt(Byte.MAX_VALUE);
+		assertEquals("Serialization - deserialization of byte failed", by, (byte) serializer.deserialize(serializer.serialze(by), byte.class));
+
+		short s = (short) random.nextInt(Short.MAX_VALUE);
+		assertEquals("Serialization - deserialization of short failed", s, (short) serializer.deserialize(serializer.serialze(s), short.class));
+
+		int i = random.nextInt();
+		assertEquals("Serialization - deserialization of integer failed", i, (int) serializer.deserialize(serializer.serialze(i), int.class));
+
+		long l = random.nextLong();
+		assertEquals("Serialization - deserialization of long failed", l, (long) serializer.deserialize(serializer.serialze(l), long.class));
+
+		float f = random.nextFloat();
+		assertEquals("Serialization - deserialization of float failed", f, (float) serializer.deserialize(serializer.serialze(f), float.class), 0);
+
+		double d = random.nextDouble();
+		assertEquals("Serialization - deserialization of double failed", d, (double) serializer.deserialize(serializer.serialze(d), double.class), 0);
+
+		char c = (char) random.nextInt();
+		assertEquals("Serialization - deserialization of char failed", c, (char) serializer.deserialize(serializer.serialze(c), char.class));
 	}
 
 }
