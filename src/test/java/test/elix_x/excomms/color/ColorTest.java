d5438f8d6e0aa36c538f9c064d03efca5b2002bd
Elix_x <elix.x.ggl@gmail.com>
2018-03-05 22:28:16 +0100

    Added RGBA fi tests to color tests

--- src/test/java/test/elix_x/excomms/color/ColorTest.java
+++ src/test/java/test/elix_x/excomms/color/ColorTest.java
@@ -13,6 +13,19 @@ public class ColorTest {
 	private Random random = new Random();
 
 	@Test
+	public void testRGBAFI(){
+		RGBA rgbaf = new RGBA(0.5f, 0.5f, 1f, 0.75f);
+		rgbaf = rgbaf.setGF(0.6371f);
+		assertEquals(0.6371f, rgbaf.getGF(), "Set F failed");
+		rgbaf.setRF(0.6f);
+		assertEquals(153, rgba.getRI(), "Get I failed");
+		rgbaf.setAI(204);
+		assertEquals(0.8f, rgba.getAF(), "Set I failed");
+
+		assertEquals(new RGBA(0f, 0.4f, 0.8f, 1f), new RGBA(-2, 102, 204, 279), "Int capping failed");
+	}
+
+	@Test
 	public void testColorConversion(){
 		for(int i = 0; i < 100; i++){
 			RGBA rgba = new RGBA(random.nextFloat(), random.nextFloat(), random.nextFloat(), random.nextFloat());
